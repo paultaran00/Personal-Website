a38e9d213bc66d304a724c51d56e2af446a00a72
paultaran00 <paultaran00@gmail.com>
2020-08-25 18:38:00 +0300

    Update buton.js

--- js/buton.js
+++ js/buton.js
@@ -103,4 +103,11 @@ $(".box2").hover(function(){
 
 
 
-
+var width = $('.g-recaptcha').parent().width();
+if (width < 302) {
+	var scale = width / 302;
+	$('.g-recaptcha').css('transform', 'scale(' + scale + ')');
+	$('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
+	$('.g-recaptcha').css('transform-origin', '0 0');
+	$('.g-recaptcha').css('-webkit-transform-origin', '0 0');
+}
